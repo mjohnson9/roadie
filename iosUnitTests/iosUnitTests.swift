4ba8da12509fc203730c7cc380f74c941ce92fe3
Michael Johnson <michael@johnson.computer>
2020-03-25 16:34:52 -0400

    ci: refactor to better support CI

--- iosUnitTests/iosUnitTests.swift
+++ iosUnitTests/iosUnitTests.swift
@@ -1,34 +1,33 @@
 //
-//  RoadieTests.swift
-//  RoadieTests
+//  iosUnitTests.swift
+//  iosUnitTests
 //
-//  Created by Michael Johnson on 2/12/20.
-//  Copyright © 2020 Michael Johnson. All rights reserved.
+//  Created by Michael Johnson on 3/24/20.
+//  Copyright © 2020 Roadie Class Team. All rights reserved.
 //
 
-/*import XCTest
-@testable import Roadie
+import XCTest
 
-class RoadieTests: XCTestCase {
+class IosUnitTests: XCTestCase {
 
-    override func setUp() {
+    override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
-    override func tearDown() {
+    override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
+    func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testPerformanceExample() {
+    func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
 
-}*/
+}
